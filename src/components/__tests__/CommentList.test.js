23f81dfb03279ead9981d9aef62477969a8098eb
Cameron Smith <uk.casmith@gmail.com>
2020-03-22 21:04:42 +0000

    added more tests

---
+++ src/components/__tests__/CommentList.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentList from 'components/CommentList';
+import Root from 'Root';
+
+beforeEach(() => {
+    wrapped = mount(
+        <Root>
+            <CommentList />
+        </Root>
+    );
+});
+
+it('creates one LI per comment', () => {
+
+});
