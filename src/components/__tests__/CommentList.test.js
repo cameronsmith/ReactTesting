15049964c2550539546c86b4ec79d0f1440efc7f
Cameron Smith <uk.casmith@gmail.com>
2020-03-23 23:22:03 +0000

    add redux testing + add packages

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -3,14 +3,27 @@ import { mount } from 'enzyme';
 import CommentList from 'components/CommentList';
 import Root from 'Root';
 
+let wrapped;
+
 beforeEach(() => {
+    const initialState = {
+        comments: ['Comment 1', 'Comment 2']
+    };
+
     wrapped = mount(
-        <Root>
+        <Root initialState={ initialState }>
             <CommentList />
         </Root>
     );
 });
 
 it('creates one LI per comment', () => {
+    expect(wrapped.find('li').length).toEqual(2);
+});
 
+it ('shows the text for each comment', () => {
+    const text = wrapped.render().text();
+
+    expect(text).toContain('Comment 1');
+    expect(text).toContain('Comment 2');
 });
